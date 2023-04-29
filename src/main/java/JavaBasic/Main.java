e2a6f0aea7128b4ebb9ef51e30187029115eba8c
Raiana Razzaq <raianarazzaq@gmail.com>
2023-04-29 14:13:16 -0700

    First

--- src/main/java/JavaBasic/Main.java
+++ src/main/java/JavaBasic/Main.java
@@ -4,12 +4,45 @@ public class Main {
 
     public static void main(String[] args){
 
-        Cat cat = new Cat();
-        cat.name = "Pet";
-        cat.age = 2;
-        cat.color = "Black";
+//        Cat cat = new Cat();
+//        cat.name = "Pet";
+//        cat.age = 2;
+//        cat.color = "Black";
+//
+//        cat.print();
+//
+//        Method_and_Construtor mc = new Method_and_Construtor();
+//        mc.greeting();
+//        String grt=mc.greeting02();
+//        System.out.println(grt);
+//        mc.greeting03("Raiana");
+//        String grt1=mc.greeting04("Raiana");
+//        System.out.println(grt1);
+//
+//            Employee employee1 = new Employee();
+//            employee1.eid=100;
+//            employee1.eName="Raiana";
+//            employee1.job="HR";
+//            employee1.eSal=1000;
+//
+//            Employee employee2 = new Employee();
+//            employee2.eid=102;
+//            employee2.eName="Raquib";
+//            employee2.job="Accountent";
+//            employee2.eSal=5000;
+//
+//            employee1.display();
+//            employee2.display();
 
-        cat.print();
+        //Classname referentialVariable(anything-class name starting with small letter is recommended) = new Classname ();
+        //referentialVariable.(variable/method)
+
+        Student student = new Student();
+        student.sid=100;
+        student.sname="Raiana";
+        student.grade='A';
+        student.print();
+
 
     }
 
