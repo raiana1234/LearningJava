5a27d61fb0033923d13ff2d7f97657a64831348e
Raiana Razzaq <raianarazzaq@gmail.com>
2023-04-30 09:31:37 -0700

    First

--- src/main/java/JavaBasic/Method_Overloading.java
+++ src/main/java/JavaBasic/Method_Overloading.java
@@ -12,22 +12,50 @@ public class Method_Overloading {
    Raiana- Women. Daughter, Sister, Sister in law
 
    add(10,20);
+   add(10,20,30);
+   add(10,20,30,40);
+
+   3000000000000.00
 
      */
-
-    int x, y;
-    double d;
-
+    /*Method Overloading Rules
+    *Methods names should/must be same
+    * Numbers of parameters should / must be different
+    * Data type must be different(if parameters count is same)
+    * Order of parameters must be different
+    */
     //This means it's the class variable since it's inside the class
-    void sum(){
+
 
         int x, y;
         double d;
-
         //this means it's a local variable
 
+        void sum(){
+            x = 20;
+            y = 50;
+            System.out.println(x + y);
+        }
 
+        void sum(int a, int b){
 
+            x = a;
+            y = b;
+            System.out.println(x + y);
+        }
+
+        void sum(int a, double b){
+
+            x = a;
+            d = b;
+            System.out.println(x + d);
+        }
+
+        void sum(double b, int a){
+
+            x = a;
+            d = b;
+            System.out.println(d + y);
         }
 
     }
